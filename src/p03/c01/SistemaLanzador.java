25c51ed0ebce0ad9ef28160b11bf075ce9b0f751
ddl1001 <74235662+ddl1001@users.noreply.github.com>
2021-04-12 20:48:51 +0200

    Finalizado SistemaLanzador

--- src/p03/c01/SistemaLanzador.java
+++ src/p03/c01/SistemaLanzador.java
@@ -3,23 +3,23 @@ package src.p03.c01;
 public class SistemaLanzador {
 	public static void main(String[] args) {
 		final int AFORO_MAX=50;
-		IParque parque = new Parque(AFORO_MAX); // TODO
+		IParque parque = new Parque(AFORO_MAX);
 		char letra_puerta = 'A';
 		
 		System.out.println("¡Parque abierto!");
 		
-		for (int i = 0; i < Integer.parseInt(args[0]); i++) {
+		for (int i = 0; i < 5; i++) {
 			
 			String puerta = ""+((char) (letra_puerta++));
 			
 			// Creación de hilos de entrada
 			ActividadEntradaPuerta entradas = new ActividadEntradaPuerta(puerta, parque);
+			
+			ActividadEntradaPuerta salida = new ActividadEntradaPuerta(puerta, parque);
+			
 			new Thread (entradas).start();
 			
-			// 
-			// TODO
-			//
-			
+			new Thread(salida).start();
 			
 		}
 	}	
